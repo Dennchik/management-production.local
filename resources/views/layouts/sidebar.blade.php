aed0b9d0d9d3a6a79048e6dc322061e61e05f7a6
Alexandr <104072970+Dennchik@users.noreply.github.com>
2026-09-19 13:37:21 +0300

    19.09.26.13.37

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,8 @@
 		<nav class="sidebar__nav">
 			@php $user = auth()->user(); @endphp
 
-			<a class="sidebar__link {{ request()->routeIs('dashboard') ? 'is-active' : '' }}" href="{{ route('dashboard') }}">
+			<a class="sidebar__link {{ request()->routeIs('dashboard') ? 'is-active' : '' }}"
+					href="{{ route('dashboard') }}">
 				<i class="icon-home-com icon"></i>
 				<span>Главная</span>
 			</a>
@@ -17,29 +18,34 @@
 
 					<div class="sidebar__submenu">
 						@if ($user->may('warehouse', 'create'))
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-receipts.*') ? 'is-active' : '' }}" href="{{ route('material-receipts.index') }}">
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-receipts.*') ? 'is-active' : '' }}"
+									href="{{ route('material-receipts.index') }}">
 								<i class="icon-indent-increase icon"></i>
 								<span>Приходный ордер</span>
 							</a>
 
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-issues.*') ? 'is-active' : '' }}" href="{{ route('material-issues.index') }}">
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-issues.*') ? 'is-active' : '' }}"
+									href="{{ route('material-issues.index') }}">
 								<i class="icon-indent-decrease icon"></i>
 								<span>Расходный ордер</span>
 							</a>
 						@endif
 
-						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('warehouse.*') ? 'is-active' : '' }}" href="{{ route('warehouse.index') }}">
+						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('warehouse.*') ? 'is-active' : '' }}"
+								href="{{ route('warehouse.index') }}">
 							<i class="icon-warehouse icon"></i>
 							<span>Материалы</span>
 						</a>
 
-						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-movements.*') ? 'is-active' : '' }}" href="{{ route('material-movements.index') }}">
+						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-movements.*') ? 'is-active' : '' }}"
+								href="{{ route('material-movements.index') }}">
 							<i class="icon-recycle-arrows icon"></i>
 							<span>Движение материалов</span>
 						</a>
 
 						@if ($user->may('rolls'))
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-rolls.*') ? 'is-active' : '' }}" href="{{ route('material-rolls.index') }}">
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('material-rolls.*') ? 'is-active' : '' }}"
+									href="{{ route('material-rolls.index') }}">
 								<i class="icon-roll icon"></i>
 								<span>Рулоны</span>
 							</a>
@@ -56,7 +62,8 @@
 					</div>
 
 					<div class="sidebar__submenu">
-						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('tasks.*') ? 'is-active' : '' }}" href="{{ route('tasks.index') }}">
+						<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('tasks.*') ? 'is-active' : '' }}"
+								href="{{ route('tasks.index') }}">
 							<i class="icon-indent-increase icon"></i>
 							<span>Производственные задачи</span>
 						</a>
@@ -73,8 +80,10 @@
 
 					<div class="sidebar__submenu" data-production-operations-list>
 						@foreach ($productionOperations as $operation)
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('production.lines.show') && request()->route('productionOperation')?->id === $operation->id ? 'is-active' : '' }}" href="{{ route('production.lines.show', $operation) }}" data-production-operation-id="{{ $operation->id }}">
-								<i class="icon-factory icon"></i>
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('production.lines.show') && request()->route('productionOperation')?->id === $operation->id ? 'is-active' : '' }}"
+									href="{{ route('production.lines.show', $operation) }}"
+									data-production-operation-id="{{ $operation->id }}">
+								<i class="icon-recycle-arrows icon"></i>
 								<span>{{ $operation->name }}</span>
 							</a>
 						@endforeach
@@ -91,14 +100,16 @@
 
 					<div class="sidebar__submenu">
 						@if ($user->may('materials'))
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('materials.*') ? 'is-active' : '' }}" href="{{ route('materials.index') }}">
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('materials.*') ? 'is-active' : '' }}"
+									href="{{ route('materials.index') }}">
 								<i class="icon-warehouse icon"></i>
 								<span>Материалы</span>
 							</a>
 						@endif
 
 						@if ($user->may('operations'))
-							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('production.operations.*') ? 'is-active' : '' }}" href="{{ route('production.operations.index') }}">
+							<a class="sidebar__link sidebar__link--submenu {{ request()->routeIs('production.operations.*') ? 'is-active' : '' }}"
+									href="{{ route('production.operations.index') }}">
 								<i class="icon-factory icon"></i>
 								<span>Технологические линии</span>
 							</a>
@@ -120,7 +131,8 @@
 			@endif
 
 			@if ($user?->may('users'))
-				<a class="sidebar__link {{ request()->routeIs('users.*') || request()->routeIs('roles.*') ? 'is-active' : '' }}" href="{{ route('users.index') }}">
+				<a class="sidebar__link {{ request()->routeIs('users.*') || request()->routeIs('roles.*') ? 'is-active' : '' }}"
+						href="{{ route('users.index') }}">
 					<i class="icon-users-group icon"></i>
 					<span>Пользователи и права</span>
 				</a>
